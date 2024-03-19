09533d3f6ecffb404f614bcd144477a905dd49b7
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2024-03-19 23:36:43 +0900

    Updated week3-pre

---
+++ week3/1.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+t.shape('turtle')
+
+t.shapesize(3)
+
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.setheading(180)
+t.forward(400)
+t.home()
+
+input()
---
+++ week3/2.py
@@ -0,0 +1,19 @@
+import turtle as t
+
+t.shape('turtle')
+t.shapesize(3)
+t.color('gray')
+t.width(3)
+t.pencolor('black')
+
+# 사각형 그리기
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+
+input()

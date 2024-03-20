08e4e02e9c31d4dd2d2fad8342ede037caf18933
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2024-03-20 13:13:07 +0900

    Updated week3

---
+++ week3/2_class.py
@@ -0,0 +1,55 @@
+# print(type(100))
+# print(type(3.44))
+# print(type("파이썬"))
+# print(type(True))
+
+# #########################
+
+# print("숭실" + str(100))
+# print("숭실"*10)
+
+# #########################
+
+# a = 100
+# b = 200
+# c,d = 300, 400
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# #########################
+
+# import keyword
+
+# print(keyword.kwlist)
+
+# #########################
+
+# x = 5
+# x += 5
+# print(x)
+
+# #########################
+
+# name = input("당신의 이름을 입력: ")
+# print(type(name))
+# print(name)
+
+# age = int(input("당신의 나이를 입력: "))
+# print(type(age))
+# print(age)
+
+# grade = float(input("당신의 학점을 입력:"))
+# print(type(grade))
+# print(grade)
+
+# #########################
+
+# 이름 = "홍길동"
+# 나이 = 20
+# 키 = 180.5
+
+# print("나는 키가 ", 키, "cm인 ", 나이, "살의 ", 이름, "입니다.")
+# print(f"나는 키가 {키}cm인 {나이}살의 {이름}입니다.")
+# print("나는 키가 %.1fcm인 %d살의 %s입니다."%(키, 나이, 이름))

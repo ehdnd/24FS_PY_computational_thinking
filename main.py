02113782269fc1d9ad95b98007c06b18293c5188
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2024-05-30 16:50:52 +0900

    Updated team_project files

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from function.signup import *
-from function.signin import *
-from function.time_taple import *
-from function.memo import *
-from function.todo_list import *
+import openai
 
+OPENAI_YOUR_KEY = "sk-proj-lsFSdcytOyeSgcUkY3UPT3BlbkFJ3Rvko1uAMcy5o2jBdNI4"
+openai.api_key = OPENAI_YOUR_KEY
+
+MODEL = "gpt-3.5-turbo"
+USER_INPUT_MSG = "chatGPT에 대해 설명해줘."
+
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": USER_INPUT_MSG}, 
+        {"role": "assistant", "content": "Who's there?"},
+    ],
+    temperature=0,
+)
+
+response

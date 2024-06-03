f7f12ab985c3fac860cbe9ae255dd118f0e92942
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2024-06-03 10:06:24 +0900

    Updated start files

--- team_project/main_files/main.py
+++ team_project/main_files/main.py
@@ -1,33 +1,28 @@
-from gpt_diary_to_emotion import diary_to_emotion
-from scrape_melon_chart import get_genre_all_songs
-import random
-
-emotion = diary_to_emotion()
-# emotion = "else인 경우확인"
-# emotion = '즐거움'
-
-print(emotion)
-
-if emotion in ('기쁨', '즐거움'):
-    num = 200
-elif emotion == '슬픔':
-    num = 100
-elif emotion == '분노':
-    num = 600
-elif emotion == '두려움':
-    num = 500
-elif emotion == '혐오':
-    num = 300
-elif emotion == '놀람':
-    num = 100
-elif emotion == '외로움':
-    num = 400
-else:
-    print("gpt 오류")
-    num = None
-
-all_songs = get_genre_all_songs(num)
-
-if all_songs != []:
-    songs = random.choice(all_songs)
-    print(f"[{songs['rank']}위] 추천곡: {songs['title']}, 아티스트: {songs['artist']}")
+while 1:
+    print("=" * 20)
+    print("기능 선택")
+    print("1. 일기 작성")
+    print("2. 일기 요약")
+    print("3. 음악 추천")
+    print("4. 프로그램 종료")
+    print("=" * 20)
+    main_menu_num = input("번호 입력: ")
+    print("-" * 20)
+    
+    if main_menu_num == "1":
+        # 일기 작성 def 필요
+        pass
+    elif main_menu_num == "2":
+        # 일기 불러오는 def 필요
+        # 일기 요약 def(불러온 일기) 필요
+        pass
+    elif main_menu_num == "3":
+        while 1:
+            print("[3. 음악추천] 불러올 일기 선택")
+            # 일기 목록 출력; len으로 반복실행
+            # 일기 불러오는 def 필요                
+            # 음악 추천 def(불러온 일기) 필요
+            break
+    else:
+        print("프로그램을 종료합니다.")
+        break
